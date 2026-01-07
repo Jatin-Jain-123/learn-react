8a7e9c34ffb46ee5cb2f26a55143cd854b59af55
Jatin Jain <jatin321jain@gmail.com>
2026-01-08 05:07:25 +0530

    Day 4 of Learning React, made a small project to understand state in React and it's relation with UI, the working of useState React hook

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,32 +4,41 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  let counter1=1;
+  let [counter2, setCounter2] = useState(1);
+  const increaseCounter1 = ()=>{
+    counter1 = counter1+1;
+    console.log(`Updated Value of counter1 is ${counter1}`);
+  }
+  const decreaseCounter1 = ()=>{
+    counter1 = counter1-1;
+    console.log(`Updated Value of counter1 is ${counter1}`);
+  }
+  const increaseCounter2 = ()=>{
+    if(counter2>=10){}
+    else{
+    counter2 = counter2+1;
+    setCounter2(counter2);
+    console.log(`Updated Value of counter2 is ${counter2}`);
+  }
+  }
+  const decreaseCounter2 = ()=>{
+    if(counter2<=0){
+    }else{
+    setCounter2(counter2-1);
+    console.log(`Updated Value of counter2 is ${counter2}`);
+    // setState is asynchronous , which causes the mismatch between UI and console values
+  }
+  }
+  return <>
+  <h1>Counter 1: {counter1}</h1>
+  <h1>Counter 2 : {counter2}</h1>
+  <button onClick= {increaseCounter1}>Increase Counter 1</button>
+  <button onClick= {decreaseCounter1}>Decrease Counter 1</button>
+  <button onClick= {increaseCounter2}>Increase Counter 2</button>
+  <button onClick= {decreaseCounter2}>Decrease Counter 2</button>
+  </>
+  
 }
 
 export default App
